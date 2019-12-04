523edeec140047d34b52d4f41d085666afb19493
yukimizake <oryx31@yahoo.fr>
2019-12-04 19:46:23 +0100

    Added key-press debouncing
    The 'received' trigger will be sent only once when a remote control button is held pushed.  That's useful for flip-flop behaviours or any action that is meant to be triggered once.

--- native/sniffer.cpp
+++ native/sniffer.cpp
@@ -1,7 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 #include "../rc-switch/RCSwitch.h"
+
+#define __DEBOUNCE_TIMESPAN_S__ 0.5d // Debounce duration in seconds (double)
+
+struct timespec previous_timestamp;
+int previous_value = -1;
+
+double get_time_diff(timespec past_date)
+{
+  double elapsed_s;
+  struct timespec now_date;
+  clock_gettime(CLOCK_MONOTONIC, &now_date);
+  elapsed_s = (now_date.tv_sec - past_date.tv_sec);
+  elapsed_s += (now_date.tv_nsec - past_date.tv_nsec) / 1000000000.0;
+  return elapsed_s;
+}
 
 int main(int argc, char *argv[])
 {
@@ -14,16 +30,29 @@ int main(int argc, char *argv[])
   RCSwitch mySwitch = RCSwitch();
   mySwitch.enableReceive(2);
 
+  // Sets 'previous_timestamp' to current time
+  clock_gettime(CLOCK_MONOTONIC, &previous_timestamp);
+
   while (1)
   {
     if (mySwitch.available())
     {
       int value = mySwitch.getReceivedValue();
-      printf("%i\n", value);
-      fflush(stdout);
-      mySwitch.resetAvailable();
-    }
 
+      // Checks if debounce timer is elapsed or if the recieved value is a new one
+      if (get_time_diff(previous_timestamp) > __DEBOUNCE_TIMESPAN_S__ || previous_value != value)
+      {
+        printf("%i\n", value);
+        fflush(stdout);
+        previous_value = value;
+      }
+
+      mySwitch.resetAvailable();
+
+      // Resets the debounce timer
+      clock_gettime(CLOCK_MONOTONIC, &previous_timestamp);
+    }
     usleep(100);
+    
   }
 }
