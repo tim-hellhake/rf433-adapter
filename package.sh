4f3b62953700fc21bd715b5fd77f9779556da41a
Michael Stegeman <michael@stegeman.me>
2020-02-26 10:19:31 -0900

    Update package.sh for new addon-builder architecture.

--- package.sh
+++ package.sh
@@ -8,15 +8,21 @@ cd ..
 
 npm install
 npm run build
+
 rm -rf node_modules
+
 if [ -z "${ADDON_ARCH}" ]; then
   TARFILE_SUFFIX=
 else
   TARFILE_SUFFIX="-${ADDON_ARCH}"
 fi
 
-sha256sum package.json manifest.json lib/*.js native/sniffer LICENSE README.md > SHA256SUMS
+shasum --algorithm 256 manifest.json package.json lib/*.js native/sniffer LICENSE README.md > SHA256SUMS
+
 TARFILE=`npm pack`
 TARFILE_ARCH="${TARFILE/.tgz/${TARFILE_SUFFIX}.tgz}"
 mv ${TARFILE} ${TARFILE_ARCH}
-echo "Created ${TARFILE_ARCH}"
+
+shasum --algorithm 256 ${TARFILE_ARCH} > ${TARFILE_ARCH}.sha256sum
+
+rm -rf SHA256SUMS
